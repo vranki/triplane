vranki/triplane

commit 8172a86502ecf3acd8e58ec7818d01419f52c87d

Use mkdir instead of install -D for FreeBSD compatibility

Makefile
@@ -72,8 +72,10 @@ tools/dksbuild: src/tools/dksbuild/dksbuild.cc
 	$(CXX) -o tools/dksbuild -g src/tools/dksbuild/dksbuild.cc
 
 install:
-	install -D triplane-classic $(DESTDIR)$(PREFIX)/games/triplane-classic
-	install -D -m 644 fokker.dks $(DESTDIR)$(PREFIX)/share/games/triplane-classic/fokker.dks
+	mkdir -p $(DESTDIR)$(PREFIX)/games/triplane-classic
+	install triplane-classic $(DESTDIR)$(PREFIX)/games/triplane-classic
+	mkdir -p $(DESTDIR)$(PREFIX)/share/games/triplane-classic
+	install -m 644 fokker.dks $(DESTDIR)$(PREFIX)/share/games/triplane-classic/fokker.dks
 test:
 	if [ ! -d triplane-testsuite ]; then echo Please darcs get http://iki.fi/lindi/darcs/triplane-testsuite; false; fi
 	bash tools/run-all-tests tools/run-one-test ./triplane-classic triplane-testsuite

commit f557611ef8ce9b38c5606b8bd5ca8cfe97cb791d

Make sure make install uses the games/ directory
This fixes bug introduced in 8172a86502.

Makefile
@@ -72,7 +72,7 @@ tools/dksbuild: src/tools/dksbuild/dksbuild.cc
 	$(CXX) -o tools/dksbuild -g src/tools/dksbuild/dksbuild.cc
 
 install:
-	mkdir -p $(DESTDIR)$(PREFIX)/games/triplane-classic
+	mkdir -p $(DESTDIR)$(PREFIX)/games
 	install triplane-classic $(DESTDIR)$(PREFIX)/games/triplane-classic
 	mkdir -p $(DESTDIR)$(PREFIX)/share/games/triplane-classic
 	install -m 644 fokker.dks $(DESTDIR)$(PREFIX)/share/games/triplane-classic/fokker.dks